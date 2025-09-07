148416ec22760328fe5b9c7520a375e8c8b12b1c
skytechrick <ricksarkar2005@gmail.com>
2025-09-07 20:15:46 +0530

    feat: model route add

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -11,4 +11,32 @@ apiRouter.get('/', (req, res) => {
     });
 });
 
+apiRouter.post("/model", async (req, res) => {
+    const { lat, long } = req.body;
+    console.log(lat, long);
+    let data = null;
+    try {
+
+        // const response = await fetch({})
+        // data = await response.json();
+
+    } catch (error) {
+        console.error("Error fetching data from AI model:", error);
+        return res.status(500).json({
+            success: false,
+            status: "error",
+            statusCode: 500,
+            message: "Failed to fetch data from AI model"
+        });
+    }
+
+    return res.json({
+        success: true,
+        status: "success",
+        statusCode: 200,
+        message: "AI endpoint hit successfully"
+        // response,
+    });
+});
+
 export default apiRouter;
